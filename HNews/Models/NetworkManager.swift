5009d66e7580f36ed73eca030720afceaeb228e2
Donna <donnajun1214@gmail.com>
2022-06-20 09:24:07 +0900

    fix stupid mistake...

--- HNews/Models/NetworkManager.swift
+++ HNews/Models/NetworkManager.swift
@@ -13,11 +13,14 @@ class NetworkManager: ObservableObject{
     
     func fetchData(){
         if let url = URL(string:"http://hn.algolia.com/api/v1/search?tags=front_page") {
+            
             let session = URLSession(configuration:.default)
             let task = session.dataTask(with: url) { data, response, error in
-                if error != nil{
+                if error == nil{
                     let decoder = JSONDecoder()
                     if let safeData = data{
+                        
+                        print(safeData)
                         do{
                             let result = try  decoder.decode(Results.self, from: safeData)
                             DispatchQueue.main.async {
